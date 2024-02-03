633b0bc003e011dff89ce27f45ba9e23c5e9bfb1
wonbin5264 <dkxlmalg@gmail.com>
2024-02-03 19:32:36 +0900

    ➕ Add TailWindCSS

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "index.css";
+
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
