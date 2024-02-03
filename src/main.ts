516d52b94f123779a628ba212a7ca20015d90293
wonbin5264 <dkxlmalg@gmail.com>
2024-02-03 19:23:46 +0900

    ➕ Add Fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUser);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
